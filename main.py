6496649bcc2e82800e4b32d302796dcd96dfef74
AK9dev <ak9dev@gmail.com>
2021-06-24 17:23:49 -0700

    Update main.py
    started data collection process by intaking songs into an array based on the directory they are stored in

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 # importing libraries
 import os
 
+wrkDir = str(input("Enter the path of your playlist folder: "))
+
+playlist = []
+
+for entity in os.listdir(wrkDir):
+    full_entity = os.path.join(wrkDir, entity)
+    if os.path.isfile(full_entity):
+        playlist.append(full_entity)
+
+
+for thing in playlist:
+    print(thing)
+
