1398265b9909f276af14cc0af95d20db7634bf64
akammela <ak9dev@gmail.com>
2021-08-05 11:32:30 -0700

    Completed Wav conversion

--- main.py
+++ main.py
@@ -6,16 +6,20 @@ import csv
 import shutil
 from pydub import AudioSegment
 import pandas as pd
+from sksound.sounds import Sound
 
 storageDir = 'sonicCompiler_lib'
 playlist = []
 ingestionComplete = False
 
+# test lib /Users/akhilk/Documents/songtooltest
+
 # --------------Initialization--------------------------
+
 while True:
     usrInput = str(input("Enter the path of your playlist folder: "))
     if os.path.exists(usrInput):
-        usrDir = input
+        usrDir = usrInput
         break
     else:
         print('file or directory does not exist, try again')
@@ -29,7 +33,7 @@ if not os.path.exists(storageDir + '/songData.csv'):
     songDataCSV.to_csv(storageDir + '/songData.csv')
 
 # Copies user's files to the application dir
-for file in os.listdir(usrDir):
+for file in os.listdir(path=usrDir):
     oldFile = usrDir + '/' + file
     newFile = storageDir + '/' + file
     shutil.copyfile(oldFile, newFile)
@@ -38,13 +42,27 @@ for file in os.listdir(usrDir):
 for entity in os.listdir(storageDir):
     full_entity = os.path.join(storageDir, entity)
     if os.path.isfile(full_entity):
-        playlist.append(full_entity)  # adds song paths to playlist
+        if '.DS_Store' not in full_entity:
+            if 'csv' not in full_entity:
+                playlist.append(full_entity)  # adds song paths to playlist
 
 # check for proper song ingestion
-for thing in playlist:  
+for thing in playlist:
     print(thing)
+
+print('-------------')
 
 # -----------Song File Conversion-------------------------
 for thing in playlist:  # convert mp3 to wav for data ingestion
     file = AudioSegment.from_mp3(thing)
+    thing = thing.replace('.mp3', '.wav')
     file.export(thing, format="wav")
+    print(thing)
+
+# get rid of any remaining mp3 files
+for misfile in os.listdir(storageDir):
+    if misfile.endswith('mp3'):
+        os.remove(os.path.join(storageDir, misfile))
+
+# --------------Sci-kit Song---------------------------
+
